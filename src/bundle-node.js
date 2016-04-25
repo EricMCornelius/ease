c8198a5a628f29b6bbb02747592b7a739a0c96f5
Eric Cornelius <ericmcornelius@gmail.com>
2016-04-25 11:24:58 -0400

    Split into bundle-web and bundle-node

--- src/bundle-node.js
+++ src/bundle-node.js
@@ -13,7 +13,9 @@ let filename = path.basename(pathname);
 let libname = filename.split('.')[0];
 
 const formatter = (percentage, message) => {
-  process.stdout.write(`\r${(100.0 * percentage).toFixed(1)}%: ${message}                 `);
+  process.stdout.clearLine();
+  process.stdout.cursorTo(0);
+  process.stdout.write(`${(100.0 * percentage).toFixed(1)}%: ${message}`);
 };
 
 const babel_opts = {
@@ -36,6 +38,7 @@ patcher(transformer, resolver);
 
 webpack({
   entry,
+  devtool: 'cheap-module-source-map',
   output: {
     path: directory,
     filename: filename,
@@ -45,14 +48,20 @@ webpack({
   resolveLoader: {
     root: path.resolve(__dirname, '../node_modules')
   },
+  resolve: {
+    modulesDirectories: ['node_modules']
+  },
   externals: [
     {
-      './external': 'external'
+      'external': true
     }
   ],
   target: 'node',
   plugins: [
     new webpack.ProgressPlugin(formatter),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': '"production"'
+    }),
     new webpack.optimize.DedupePlugin(),
     new webpack.optimize.UglifyJsPlugin()
   ],
@@ -65,8 +74,20 @@ webpack({
         query: babel_opts
       },
       {
+        test: /\.s?css$/,
+        loaders: ['style', 'css', 'sass']
+      },
+      {
         test: /\.json$/,
         loaders: ['json']
+      },
+      {
+        test: /\.yaml$/,
+        loaders: ['json', 'yaml']
+      },
+      {
+        test: /\.txt$/,
+        loaders: ['raw']
       }
     ]
   }
---
+++ src/bundle-web.js
@@ -0,0 +1,99 @@
+#!/usr/bin/env node
+
+import setpath from './setpath';
+
+import webpack from 'webpack';
+import path from 'path';
+import patcher from './module_patch';
+
+let entry = path.resolve(process.argv[2]);
+let pathname = path.resolve(process.argv[3]);
+let directory = path.dirname(pathname);
+let filename = path.basename(pathname);
+let libname = filename.split('.')[0];
+
+const formatter = (percentage, message) => {
+  process.stdout.clearLine();
+  process.stdout.cursorTo(0);
+  process.stdout.write(`${(100.0 * percentage).toFixed(1)}%: ${message}`);
+};
+
+const babel_opts = {
+  babelrc: false,
+  presets: ['es2015', 'react', 'stage-2'],
+  plugins: ['syntax-decorators', 'transform-decorators-legacy']
+};
+
+let transformer = (content, filename) => {
+  return content;
+};
+
+let resolver = (request, parent) => {
+  return (request.startsWith('babel-preset') || request.startsWith('babel-plugin')) ?
+    path.resolve(__dirname, '../node_modules/', request) :
+    request;
+}
+
+patcher(transformer, resolver);
+
+webpack({
+  entry,
+  devtool: 'cheap-module-source-map',
+  output: {
+    path: directory,
+    filename: filename
+  },
+  resolveLoader: {
+    root: path.resolve(__dirname, '../node_modules')
+  },
+  resolve: {
+    moduleDirectories: ['node_modules', 'bower_components']
+  },
+  externals: [
+    {
+      'external': true,
+      'regenerator': true
+    }
+  ],
+  // target: 'node',
+  plugins: [
+    new webpack.ProgressPlugin(formatter),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': '"production"'
+    }),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin()
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: babel_opts
+      },
+      {
+        test: /\.s?css$/,
+        loaders: ['style', 'css', 'sass']
+      },
+      {
+        test: /\.json$/,
+        loaders: ['json']
+      },
+      {
+        test: /\.yaml$/,
+        loaders: ['json', 'yaml']
+      },
+      {
+        test: /\.txt$/,
+        loaders: ['raw']
+      }
+    ]
+  }
+}, (err, stats) => {
+  if (err) {
+    throw err;
+  }
+
+  console.log(stats.toString('normal'));
+});
