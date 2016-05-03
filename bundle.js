16c2ca8cf5236f378365dcc017583c25a5005476
Eric Cornelius <ericmcornelius@gmail.com>
2016-05-03 13:59:17 -0400

    Prep publishable bundle

---
+++ bundle.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env ease-run
+
+import fs from 'fs';
+
+const deps = fs.readdirSync('node_modules');
+const package_json = JSON.parse(fs.readFileSync('package.json'));
+const ignored = {
+  '.bin': true,
+  'node-sass': true
+};
+
+package_json.bundledDependencies = deps.filter(dep => ignored[dep] === undefined);
+fs.writeFileSync('package.json', JSON.stringify(package_json, null, 2));
